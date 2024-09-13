3f625314bd6f2cf0e6124251234161d61e611771
OlesiaKubska <kublesia0908@gmail.com>
2024-09-13 18:47:34 +0200

    Added translations for Education section in EN, PL, and UK

--- src/pages/Education/Education.jsx
+++ src/pages/Education/Education.jsx
@@ -5,33 +5,30 @@ import {
  Info,
  Item,
 } from "./Education.styled";
+import { useTranslation } from "react-i18next";
 
 const Education = () => {
+ const { t } = useTranslation();
+
  return (
   <EducationContainer>
-   <Title>Education</Title>
+   <Title>{t("educationTitle")}</Title>
    <University>
     <Item>
-     Bialystok University of Technology | Poland
-     <Info>“JavaScript Developer” | October 2023 - July 2024</Info>
+     {t("bialystokUniversity")}
+     <Info>{t("jsDeveloperCourse")}</Info>
     </Item>
     <Item>
-     IT School GolT | Ukraine
-     <Info>“Full Stack Developer” | 2023 – 2024 | CERTIFICATE</Info>
+     {t("golTSchool")}
+     <Info>{t("fullStackCourse")}</Info>
     </Item>
     <Item>
-     National Pedagogical Dragomanov University | Ukraine
-     <Info>
-      “Education institution management” | 2014 – 2016 | MASTER | Business,
-      office and organization manager
-     </Info>
+     {t("dragomanovUniversityManagement")}
+     <Info>{t("managementCourse")}</Info>
     </Item>
     <Item>
-     National Pedagogical Dragomanov University | Ukraine
-     <Info>
-      “Musical art” | 2010 - 2015 | MASTER | Methods of musical education
-      teacher (lecturer), music teacher and choir master
-     </Info>
+     {t("dragomanovUniversityMusic")}
+     <Info>{t("musicCourse")}</Info>
     </Item>
    </University>
   </EducationContainer>
